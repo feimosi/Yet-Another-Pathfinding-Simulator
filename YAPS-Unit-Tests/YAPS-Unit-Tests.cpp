90e30da909f0690ac466576409eaa2cb46c8f6e7
Marek Grzybek <feimosi@onet.pl>
2014-05-20 13:14:05 +0200

    More unit tests

--- YAPS-Unit-Tests/YAPS-Unit-Tests.cpp
+++ YAPS-Unit-Tests/YAPS-Unit-Tests.cpp
@@ -9,17 +9,30 @@
 using namespace yaps;
 
 TEST(DataMatrixTest, insertAndGetValue) {
-	DataMatrix<float> array(2, 2);
+	DataMatrix<float> array(3, 3);
 	array[0][0] = 20.f;
+	array[2][2] = 15.8f;
+	array[1][2] = .8f;
 	ASSERT_EQ(array[0][0], 20);
-	ASSERT_EQ(array[1][1], 0);
-	ASSERT_ANY_THROW(array[4][0]);
-	ASSERT_ANY_THROW(array[0][6]);
+	ASSERT_EQ(array[2][2], 15.8f);
+	ASSERT_EQ(array[1][2], .8f);
+}
+
+TEST(DataMatrixTest, referToOutOfBoundIndex) {
+	DataMatrix<float> array(2, 2);
+	ASSERT_ANY_THROW(array[2][2]);
+	ASSERT_ANY_THROW(array[100][0]);
+	ASSERT_ANY_THROW(array[0][100]);
+	ASSERT_ANY_THROW(array[200][200]);
 }
 
 TEST(SimulatorTest, readDataFromFile) {
-	Simulator simulator(4, 4);
+	Simulator simulator(5, 5);
+	simulator.initialise("data.txt");
+}
+
+TEST(SimulatorTest, approximateData) {
+	Simulator simulator(5, 5);
 	simulator.initialise("data.txt");
 	simulator.run();
-	simulator.printCurrentData();
 }
