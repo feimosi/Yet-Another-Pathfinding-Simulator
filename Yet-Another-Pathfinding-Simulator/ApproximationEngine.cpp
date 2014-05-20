5a22c98742fa21ae98f445106486dcd362aff61d
Marek Grzybek <feimosi@onet.pl>
2014-05-20 13:03:40 +0200

    Implemented 'Inverse distance weighting' interpolation for approximating data

--- Yet-Another-Pathfinding-Simulator/ApproximationEngine.cpp
+++ Yet-Another-Pathfinding-Simulator/ApproximationEngine.cpp
@@ -3,10 +3,64 @@
 
 using namespace yaps;
 
-ApproximationEngine::ApproximationEngine() {
-
+void ApproximationEngine::prepareData() {
+	for (int i = data.getHeight() - 1; i >= 0; i--) {
+		for (int j = data.getWidth() - 1; j >= 0; j--) {
+			if (data[i][j] != NULL) {
+				paramX.push_back(j);
+				paramY.push_back(i);
+				valueF.push_back(data[i][j]);
+			}
+		}
+	}
 }
 
-ApproximationEngine::~ApproximationEngine() {
+float ApproximationEngine::calculateWeighting(unsigned xi, unsigned yi, unsigned xj, unsigned yj) {
+	/* 
+		w = 1 / d^p 
+		where d - Euclidean distance between 2 points and p - power parameter
+	*/
+	return 1 / pow(static_cast<float>( sqrt((xi - xj) * (xi - xj) + (yi - yj) * (yi - yj)) ), POWER_PARAMETER);
+}
 
+float ApproximationEngine::interpolate(unsigned x, unsigned y) {
+	float dividend, divisor;	
+	float f, w;				// Temp for function value and weighting for every point
+	unsigned px, py;		// Temp for every point coordinates
+	dividend = divisor = 0;		
+
+	// Initialise vector iterators
+	auto itX = paramX.cbegin(),
+		 itY = paramY.cbegin();
+	auto itF = valueF.cbegin();
+	
+	// paramX, paramY, valueF have the same size
+	while (itF != valueF.cend()) {
+		px = *itX;
+		py = *itY;
+		f = *itF;
+		itX++, itY++, itF++;
+
+		w = calculateWeighting(px, py, x, y);
+		dividend += w * f;
+		divisor += w;
+	}
+
+	return divisor != 0 ? dividend / divisor : data[x][y];
+}
+
+ApproximationEngine::ApproximationEngine(DataMatrix<float> &dataRef) : data(dataRef) { }
+
+ApproximationEngine::~ApproximationEngine() { }
+
+DataMatrix<float> &ApproximationEngine::approximate() {
+	prepareData();
+	for (int i = data.getHeight() - 1; i >= 0; i--) {
+		for (int j = data.getWidth() - 1; j >= 0; j--) {
+			if (data[i][j] == NULL) {
+				data[i][j] = interpolate(j, i);
+			}
+		}
+	}
+	return data;
 }
--- Yet-Another-Pathfinding-Simulator/ApproximationEngine.h
+++ Yet-Another-Pathfinding-Simulator/ApproximationEngine.h
@@ -1,12 +1,62 @@
 #pragma once
 
+/*
+	TODO:
+	Implement Modified Shepard's Method
+*/
+
 namespace yaps {
 
+	/**
+	 *	Class which interpolates 2D data with a known scattered set of points.
+	 *	For now it uses Inverse distance weighting (IDW) algorithm, Shepard's method.
+	 *	@link http://en.wikipedia.org/wiki/Inverse_distance_weighting
+	 */
 	class ApproximationEngine {
+		const int POWER_PARAMETER = 8;	// Shepard's method power parameter
+		DataMatrix<float> &data;		// Reference to data matrix that should be interpolated (values 0 are considered missing)
+		std::vector<unsigned> paramX;	// Vector of function x arguments
+		std::vector<unsigned> paramY;	// Vector of function y arguments
+		std::vector<float> valueF;		// Vector of function values in x, y
+
+		/**
+		 *	Fill the vectors with appropriate data, such as every index corresponds certain function value in x, y
+		 */
+		void prepareData();
+		
+		/**
+		 *	Calculate weighting parameter from Shepard's method
+		 *	@param xi x value of the first point
+		 *	@param yi y value of the first point
+		 *	@param xj x value of the second point
+		 *	@param yj y value of the second point
+		 *	@return value of weighting
+		 */
+		float calculateWeighting(unsigned, unsigned, unsigned, unsigned);
+
+		/**
+		 *	Calculate interpolation value for a certain point
+		 *	@param x
+		 *	@param y
+		 */
+		float interpolate(unsigned, unsigned);
 	public:
-		ApproximationEngine();
+		/**
+		 *	Constructor
+		 *	@param riverBottomRef Init value for riverBottom
+		 */
+		ApproximationEngine(DataMatrix<float> &);
+		
+		/**
+		 *	Destructor
+		 */
 		~ApproximationEngine();
-		float **approximate(float **) { return NULL; }
+		
+		/**
+		 *	Prepare data and run interpolation algorithm
+		 *	@return data matrix filled with approximated values
+		 */
+		DataMatrix<float> &approximate();
 	};
 
 }
