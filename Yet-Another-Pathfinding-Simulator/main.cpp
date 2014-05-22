69f689d49d2943e2f92f08409a20791beb380d80
Marek Grzybek <feimosi@onet.pl>
2014-05-22 20:57:16 +0200

    Running test

--- Yet-Another-Pathfinding-Simulator/main.cpp
+++ Yet-Another-Pathfinding-Simulator/main.cpp
@@ -1,11 +1,19 @@
 #include "stdafx.h"
 #include "Simulator.h"
+#include "GUIView.h"
 
 using namespace yaps;
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	cout << "Test";
+	Simulator simulator(7, 7);
+	simulator.initialise("data.txt");
+	simulator.printCurrentData();
+	cout << "\n---------------\n" << flush;
+	simulator.run();
+	simulator.printCurrentData();
+	GUIView gui;
+	gui.run();
 	int x;
 	cin >> x;
 }
