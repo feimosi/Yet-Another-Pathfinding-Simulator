0990dceebd251209864a9123158ba460fd082499
Marek Grzybek <feimosi@onet.pl>
2014-05-22 20:48:29 +0200

    New dependencies added

--- Yet-Another-Pathfinding-Simulator/stdafx.h
+++ Yet-Another-Pathfinding-Simulator/stdafx.h
@@ -1,8 +1,10 @@
 // Precompiled Header
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <utility>
 #include <list>
 #include <vector>
+#include <SFML\Graphics.hpp>
 #include "DataMatrix.h"
