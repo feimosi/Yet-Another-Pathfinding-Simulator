d37ceec71981cd278643f1671ece616230ac6649
Marek Grzybek <feimosi@onet.pl>
2014-05-23 17:48:47 +0200

    New organization of public methods for handling images and text files and file detection

--- Yet-Another-Pathfinding-Simulator/InputCollector.cpp
+++ Yet-Another-Pathfinding-Simulator/InputCollector.cpp
@@ -3,34 +3,54 @@
 
 using namespace yaps;
 
-InputCollector::InputCollector(DataMatrix<float> &riverBottomRef) : riverBottom(riverBottomRef), errorCode(0) { }
-
-InputCollector::~InputCollector() {
-	dataFile.close();
-}
-
-bool InputCollector::openFile(std::string filePath) {
-	dataFile.open(filePath);
-	return dataFile.is_open() ? true : false;
-}
-
 bool InputCollector::loadDataFromFile() {
-	if (dataFile.is_open()) {
+	if (dataFile.is_open() && !dataFile.eof()) {
 		for (int i = 0; i < riverBottom.getHeight(); i++)
 			for (int j = 0; j < riverBottom.getWidth(); j++)
 				if (!dataFile.eof())
 					dataFile >> riverBottom[i][j];
 		dataFile.close();
-	} else 
+	}
+	else
 		return false;
 	return true;
-}
-
-bool InputCollector::openImage(std::string filePath) {
-	return dataImage.loadFromFile(filePath) ? true : false;
 }
 
 bool InputCollector::loadDataFromImage() {
 	// TODO
 	return false;
+}
+
+InputCollector::InputCollector(DataMatrix<float> &riverBottomRef) : riverBottom(riverBottomRef), errorCode(0), parseImage(false) { }
+
+InputCollector::~InputCollector() { }
+
+bool InputCollector::openFile(std::string filePath) {
+	std::smatch match;
+	std::regex_match(filePath, match, std::regex("(.*?)([^\\\\\\/]+?)\\.(jpe?g|png|txt)$")); // Test file path for regex match
+
+	// Check if regex match was successful
+	if (match.ready()) {
+		/* Regex groups:
+			match[0] - entire match
+			match[1] - path to the file (before / or \)
+			match[2] - file name
+			match[3] - file extension
+		*/
+		if (match[3] == "jpg" || match[3] == "png") {
+			parseImage = true;
+			return dataImage.loadFromFile(filePath) ? true : false;
+		} else if (match[3] == "txt") {
+			if (dataFile.is_open())
+				dataFile.close();
+			dataFile.open(filePath);
+			return dataFile.is_open() ? true : false;
+		}
+	}
+
+	return false;
+}
+
+bool InputCollector::loadData() {
+	return parseImage ? loadDataFromImage() : loadDataFromFile();
 }
