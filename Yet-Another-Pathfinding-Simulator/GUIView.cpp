4dd487c10007e843a21c1eeacb9a9404dab7fcff
Marek Grzybek <feimosi@onet.pl>
2014-05-24 17:57:18 +0200

    Initial map drawing and new data members

--- Yet-Another-Pathfinding-Simulator/GUIView.cpp
+++ Yet-Another-Pathfinding-Simulator/GUIView.cpp
@@ -3,16 +3,72 @@
 
 using namespace yaps;
 
-GUIView::GUIView() {
+GUIView::GUIView(int width, int distance, std::string dataSource) : simulator(width, distance) {
+    simulator.initialise(dataSource);
 }
 
-GUIView::~GUIView() {
+GUIView::~GUIView() { }
+
+sf::Uint8 *GUIView::generateMapImage(const DataMatrix<float> &data, sf::Uint8 *pixels) {
+    int dataWidth = data.getWidth();
+    int dataHeight = data.getHeight();
+    int x, y, index;    // Downscaled x, y coordinates
+    float cPix;         // Current pixel value according to data
+    for (int i = 0; i < MAP_HEIGHT; i++) {
+        for (int j = 0; j < MAP_WIDTH; j++) {
+            y = int(((float)i / MAP_HEIGHT) * dataWidth);
+            x = int(((float)j / MAP_WIDTH) * dataHeight);
+            index = (j + MAP_HEIGHT * i) * 4;
+            cPix = data[y][x];
+            pixels[index] = castColor(cPix, RED);       // Red channel
+            pixels[index + 1] = castColor(cPix, GREEN); // Green channel
+            pixels[index + 2] = castColor(cPix, BLUE);  // Blue channel
+            pixels[index + 3] = 255;                    // Alpha channel
+        }
+    }
+    return pixels;
+}
+
+sf::Uint8 GUIView::castColor(float value, int rgba) {
+    sf::Uint8 c;
+    switch (rgba) {
+    case 0:
+        c = 0; break;
+    case 1:
+        c = unsigned char(255 - (value / MAX_DEPTH) * 255); break;
+    case 2:
+        c = 255; break;
+    default:
+        c = 0; break;
+    }
+    return c;
 }
 
 void GUIView::run() {
-	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Yet Another Pathfinding Simulator", 
+	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Yet Another Pathfinding Simulator", 
 		sf::Style::Titlebar | sf::Style::Close);
 	sf::Event event;
+    sf::Image map;
+    sf::Texture mapTexture;
+    sf::Texture boatTexture;
+    sf::Sprite mapSprite;
+    sf::Sprite boatSprite;
+    sf::Uint8 pixels[MAP_HEIGHT * MAP_WIDTH * 4];
+
+    window.setFramerateLimit(60);  
+    simulator.run();
+
+    map.create(MAP_WIDTH, MAP_HEIGHT, generateMapImage(simulator.getRiverBottom(), pixels));
+    mapTexture.loadFromImage(map);
+    mapSprite.setTexture(mapTexture);
+    mapSprite.setPosition(WINDOW_WIDTH / 2 - MAP_WIDTH / 2, WINDOW_HEIGHT / 2 - MAP_HEIGHT / 2);
+    
+    
+    boatTexture.loadFromFile("wood.jpg");
+    boatSprite.setTextureRect(sf::IntRect(0, 0, 30, 60));
+    boatSprite.setTexture(boatTexture);
+    sf::Vector2f boatPosition(mapSprite.getPosition() + sf::Vector2f(MAP_WIDTH / 2 - boatSprite.getGlobalBounds().width / 2, MAP_HEIGHT - boatSprite.getGlobalBounds().height));
+    boatSprite.setPosition(boatPosition);
 
 	// Main application loop
 	while (window.isOpen()) {
@@ -25,7 +81,10 @@ void GUIView::run() {
 		}
 
 		// Rendering
-		window.clear();
+        window.clear();
+        window.draw(mapSprite);
+        boatSprite.move(0, -0.4f);
+        window.draw(boatSprite);
 		window.display();
 	}
 }
--- Yet-Another-Pathfinding-Simulator/GUIView.h
+++ Yet-Another-Pathfinding-Simulator/GUIView.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Simulator.h"
 
 namespace yaps {
 
@@ -6,15 +7,33 @@ namespace yaps {
 	 *  Main application window and user interface
 	 */
 	class GUIView {
-		static const int SCREEN_WIDTH = 800;
-		static const int SCREEN_HEIGHT = 600;
+        static const int WINDOW_WIDTH = 800,
+            WINDOW_HEIGHT = 600,
+            MAP_WIDTH = 200,
+            MAP_HEIGHT = 200,
+            RED = 0, GREEN = 1, BLUE = 2,
+            MAX_DEPTH = 3;
+        Simulator simulator;
+        
+        /**
+         *	Generate pixel array by upscaling data matrix and converting it to RGBA
+         */
+        sf::Uint8 *generateMapImage(const DataMatrix<float> &, sf::Uint8 *);
+        
+        /**
+         *	Convert value from [0; MAX_DEPTH] to RGBA determined by flag
+         *  @param value    Value to convert
+         *  @param rgba     Flag describing color channel
+         */
+        sf::Uint8 castColor(float, int);
 	public:
 		/**
 		 *	Constructor
-		 *	@param width	Init value for riverBottom constructor
-		 *	@param distance	Init value for riverBottom constructor
+		 *	@param width        Init value for simulator constructor
+		 *	@param distance     Init value for simulator constructor
+         *  @param dataSource   Path to the file
 		 */
-		GUIView();
+        GUIView(int, int, std::string);
 
 		/**
 		 *	Destructor
