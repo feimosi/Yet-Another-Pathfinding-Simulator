6a774757ed6da5f356e817a0cd1cadc5cb4188aa
Marek Grzybek <feimosi@onet.pl>
2014-05-24 18:11:19 +0200

    New comments added

--- Yet-Another-Pathfinding-Simulator/FuzzyControlSystem.cpp
+++ Yet-Another-Pathfinding-Simulator/FuzzyControlSystem.cpp
@@ -11,6 +11,10 @@ FuzzyControlSystem::~FuzzyControlSystem() {
 
 }
 
+bool FuzzyControlSystem::run(std::vector<int> front, std::vector<int> left, std::vector<int> right) {
+    return false;
+}
+
 FuzzyControlSystem::Fuzzifier::Fuzzifier() {
 	// Allocate memory and assign lambda functions
 	membershipFunctions = new std::function<int(int, int, int, int)>[3];
@@ -30,8 +34,8 @@ FuzzyControlSystem::Fuzzifier::Fuzzifier() {
 void FuzzyControlSystem::Fuzzifier::fuziffy(int front, int left, int right) {
 	// Calculate input relation to fuzzy sets
 	for (int i = 0; i < 3; i++) {
-		fuzzySets[FRONT][i] = membershipFunctions[i](1, frontLow, frontMedium, frontHigh);
-		fuzzySets[LEFT][i] = membershipFunctions[i](1, leftLow, leftMedium, leftHigh);
-		fuzzySets[RIGHT][i] = membershipFunctions[i](1, rightLow, rightMedium, rightHigh);
+		fuzzySets[FRONT][i] = membershipFunctions[i](1, low, medium, high);
+        fuzzySets[LEFT][i] = membershipFunctions[i](1, low, medium, high);
+        fuzzySets[RIGHT][i] = membershipFunctions[i](1, low, medium, high);
 	}
 }
--- Yet-Another-Pathfinding-Simulator/FuzzyControlSystem.h
+++ Yet-Another-Pathfinding-Simulator/FuzzyControlSystem.h
@@ -1,19 +1,37 @@
 #pragma once
 
-
 namespace yaps {
 
+    /**
+     *  Decision support system using fuzzy logic
+     *  @link http://en.wikipedia.org/wiki/Fuzzy_control_system
+     *  There are 3 input variables which can belong to 3 different fuzzy sets each, but with the same delimiters:
+     *  - Front (LOW, MEDIUM, HIGH)
+     *  - Left (LOW, MEDIUM, HIGH)
+     *  - Right (LOW, MEDIUM, HIGH)
+     *  Output: Speed and angle
+     */
 	class FuzzyControlSystem {
+
 		class Fuzzifier {
-			static const short FRONT = 0, LEFT = 1,	RIGHT = 2;	// Array indexes for specific input and fuzzy set
-			static const short LOW = 0,	MEDIUM = 1,	HIGH = 2;	// --||--
-			int frontLow, frontMedium, frontHigh;				//	|
-			int leftLow, leftMedium, leftHigh;					//	Different ranges in fuzzy sets 
-			int rightLow, rightMedium, rightHigh;				//	|
+			static const short FRONT = 0, LEFT = 1,	RIGHT = 2;  // Array indexes for specific input and fuzzy set
+			static const short LOW = 0,	MEDIUM = 1,	HIGH = 2;   // --||--
+			int low, medium, high;                              //  Different ranges in fuzzy sets
 			std::function<int(int, int, int, int)> *membershipFunctions;
-			int fuzzySets[3][3];								// 3 input variables x 3 sets
+			int fuzzySets[3][3];                                // 3 input variables x 3 sets
 		public:
+            /**
+             *	Constructor
+             *  Prepare membership functions
+             */
 			Fuzzifier();
+            
+            /**
+             *	Fuzzify selected input by calculating membership on each fuzzy set
+             *	@param front
+             *	@param left
+             *	@param right
+             */
 			void fuziffy(int, int, int);
 		};
 		class InferenceEngine;
@@ -21,9 +39,26 @@ namespace yaps {
 		class Defuzzifier;
 
 		Fuzzifier fuzzifier;
+        int speed;
+        int angle;
 	public:
-		FuzzyControlSystem();
+        /**
+         *	Constructor
+         */
+        FuzzyControlSystem();
+        
+        /**
+         *	Constructor
+         */
 		~FuzzyControlSystem();
+
+        /**
+         *	Run the whole process
+         *	@param front    Vector containing input values
+         *	@param left     --||--
+         *	@param right    --||--
+         */
+        bool run(std::vector<int>, std::vector<int>, std::vector<int>);
 	};
 
 }
