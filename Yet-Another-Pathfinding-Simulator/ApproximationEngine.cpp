9389d3832532cd996806f26e8fd839a569c7a947
Marek Grzybek <feimosi@onet.pl>
2014-05-27 22:50:08 +0200

    Better and more efficient approximation implemented

--- Yet-Another-Pathfinding-Simulator/ApproximationEngine.cpp
+++ Yet-Another-Pathfinding-Simulator/ApproximationEngine.cpp
@@ -4,46 +4,46 @@
 using namespace yaps;
 
 void ApproximationEngine::prepareData() {
-    for (int i = data.getHeight() - 1; i >= 0; i--) {
-        for (int j = data.getWidth() - 1; j >= 0; j--) {
-            if (data[i][j] != NULL) {
-                paramX.push_back(j);
-                paramY.push_back(i);
-                valueF.push_back(data[i][j]);
+    RADIUS = data.getWidth() >> 2;
+#ifdef DEBUG
+    std::cout << "---------------------------\n" << "ApproximationEngine\n\n" 
+        << "Data matrix height: " << data.getHeight() << '\n'
+        << "Data matrix width: " << data.getWidth() << '\n'
+        << "Radius: " << RADIUS << '\n';
+#endif
+    /////// DEPRECATED To remove
+    for (int yi = data.getHeight() - 1; yi >= 0; yi--) {
+        for (int xi = data.getWidth() - 1; xi >= 0; xi--) {
+            if (data[yi][xi] != NULL) {
+                paramX.push_back(xi);
+                paramY.push_back(yi);
+                valueF.push_back(data[yi][xi]);
             }
         }
     }
+    ///////
 }
 
-float ApproximationEngine::calculateWeighting(unsigned xi, unsigned yi, unsigned xj, unsigned yj) {
-    /* 
-        w = 1 / d^p 
-        where d - Euclidean distance between 2 points and p - power parameter
-    */
-    return 1 / pow(static_cast<float>( sqrt((xi - xj) * (xi - xj) + (yi - yj) * (yi - yj)) ), POWER_PARAMETER);
-}
-
-float ApproximationEngine::interpolate(unsigned x, unsigned y) {
+float ApproximationEngine::interpolate(const Coordinates &point) {
+    int x = point.x;
+    int y = point.y;
     float dividend, divisor;    // Final equation components
     float f, w;                 // Temp for function value and weighting for every point
-    unsigned px, py;            // Temp for every point coordinates
     dividend = divisor = 0;     
-
-    // Initialise vector iterators
-    auto itX = paramX.cbegin(),
-         itY = paramY.cbegin();
-    auto itF = valueF.cbegin();
+    int width = data.getWidth();
+    int height = data.getHeight();
     
     // paramX, paramY, valueF have the same size
-    while (itF != valueF.cend()) {
-        px = *itX;
-        py = *itY;
-        f = *itF;
-        itX++, itY++, itF++;
 
-        w = calculateWeighting(px, py, x, y);
-        dividend += w * f;
-        divisor += w;
+    for (int yi = (y - RADIUS < 0 ? 0 : y - RADIUS); yi <= y + RADIUS; yi++) {
+        int xi = (x - RADIUS < 0 ? 0 : x - RADIUS);
+        for (; xi <= x + RADIUS; xi++) {
+            if ( yi >= height || xi >= width || !(f = data[yi][xi]) )
+                continue;
+            w = calculateWeighting(Coordinates(xi, yi), Coordinates(x, y));
+            dividend += w * f;
+            divisor += w;
+        }
     }
 
     return divisor != 0 ? dividend / divisor : data[x][y];
@@ -55,12 +55,48 @@ ApproximationEngine::~ApproximationEngine() { }
 
 DataMatrix<float> &ApproximationEngine::approximate() {
     prepareData();
-    for (int i = data.getHeight() - 1; i >= 0; i--) {
-        for (int j = data.getWidth() - 1; j >= 0; j--) {
-            if (data[i][j] == NULL) {
-                data[i][j] = interpolate(j, i);
-            }
+    Coordinates point;
+    sf::Clock clock;
+    for (int yi = data.getHeight() - 1; yi >= 0; yi--) {
+        for (int xi = data.getWidth() - 1; xi >= 0; xi--) {
+            if (data[yi][xi] == NULL)
+                data[yi][xi] = interpolate(point.set(xi, yi));
         }
     }
+#ifdef DEBUG
+    std::cout << "Approximation time: " << clock.getElapsedTime().asSeconds() 
+        << " s\n---------------------------\n";
+#endif
     return data;
 }
+
+/////// DEPRECATED To remove
+float ApproximationEngine::old_interpolate(const Coordinates &point) {
+    int x = point.x;
+    int y = point.y;
+    float dividend, divisor;    // Final equation components
+    float f, w;                 // Temp for function value and weighting for every point
+    unsigned px, py;            // Temp for every point coordinates
+    dividend = divisor = 0;
+    int width = data.getWidth();
+    int height = data.getHeight();
+    // Initialise vector iterators
+    auto itX = paramX.cbegin(),
+        itY = paramY.cbegin();
+    auto itF = valueF.cbegin();
+
+    // paramX, paramY, valueF have the same size
+    while (itF != valueF.cend()) {
+        px = *itX;
+        py = *itY;
+        f = *itF;
+        itX++, itY++, itF++;
+
+        w = 1 / pow((distanceBetween(Coordinates(px, py), Coordinates(x, y))), POWER_PARAMETER);
+        dividend += w * f;
+        divisor += w;
+    }
+
+    return divisor != 0 ? dividend / divisor : data[x][y];
+}
+///////
--- Yet-Another-Pathfinding-Simulator/ApproximationEngine.h
+++ Yet-Another-Pathfinding-Simulator/ApproximationEngine.h
@@ -4,15 +4,18 @@ namespace yaps {
 
     /**
      *  Class which interpolates 2D data with a known scattered set of points.
-     *  For now it uses Inverse distance weighting (IDW) algorithm, Shepard's method.
+     *  For now it uses Inverse distance weighting (IDW) algorithm, Modified Shepard's method.
      *  @link http://en.wikipedia.org/wiki/Inverse_distance_weighting
      */
     class ApproximationEngine {
         const int POWER_PARAMETER = 8;  // Shepard's method power parameter
+        int RADIUS;                     // Radius in which we probe the data. For now it's 1/8 of the data width assigned in a constructor
         DataMatrix<float> &data;        // Reference to data matrix that should be interpolated (values 0 are considered missing)
+        /////// DEPRECATED To remove
         std::vector<unsigned> paramX;   // Vector of function x arguments
         std::vector<unsigned> paramY;   // Vector of function y arguments
         std::vector<float> valueF;      // Vector of function values in x, y
+        ///////
 
         /**
          *  Fill the vectors with appropriate data, such as every index corresponds certain function value in x, y
@@ -21,20 +24,36 @@ namespace yaps {
         
         /**
          *  Calculate weighting parameter from Shepard's method
-         *  @param xi x value of the first point
-         *  @param yi y value of the first point
-         *  @param xj x value of the second point
-         *  @param yj y value of the second point
+         *  @param i, j first and second point coordinates
          *  @return value of weighting
          */
-        float calculateWeighting(unsigned, unsigned, unsigned, unsigned);
+        float calculateWeighting(Coordinates i, Coordinates j) {
+            float distance = distanceBetween(i, j);
+            return pow( (RADIUS - distance) / (RADIUS * distance), 2 );
+        }
+
+        /**
+         *  Calculate distance between two points
+         *  @param i, j first and second point coordinates
+         *  @return distance between two points
+         */
+        float distanceBetween(Coordinates i, Coordinates j) {
+            return static_cast<float>(sqrt((i.x - j.x) * (i.x - j.x) + (i.y - j.y) * (i.y - j.y)));
+        }
 
         /**
          *  Calculate interpolation value for a certain point
-         *  @param x
-         *  @param y
+         *  @param point
          */
-        float interpolate(unsigned, unsigned);
+        float interpolate(const Coordinates &);
+
+        /////// DEPRECATED To remove
+        /**
+        *  Calculate interpolation with less efficient method (Deprecated)
+        *  @param point
+        */
+        float old_interpolate(const Coordinates &);
+        /////// 
     public:
         /**
          *  Constructor
