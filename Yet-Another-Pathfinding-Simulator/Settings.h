67c1648d1718dfe406cf9637d6629c78fc2da849
Marek Grzybek <feimosi@onet.pl>
2014-05-29 16:15:09 +0200

    New data member STEP

--- Yet-Another-Pathfinding-Simulator/Settings.h
+++ Yet-Another-Pathfinding-Simulator/Settings.h
@@ -12,14 +12,15 @@ namespace yaps {
                        MAP_HEIGHT,
                        BOAT_WIDTH,
                        BOAT_LENGTH,
-                       PROXIMITY;   // Number of fields distant from the boat to consider in fuzzy control system 
+                       PROXIMITY,   // Number of fields distant from the boat to consider in fuzzy control system 
+                       STEP;        // Number to fields to step forward in every simulation cycle
         const float MAX_ANGLE,
                     MAX_SPEED;
         float getMaxDepth() { return maxDepth; }
         void setMaxDepth(float newMaxDepth) { maxDepth = newMaxDepth; }
 
-        Settings(unsigned mapWidth, unsigned mapHeight, unsigned boatWidth = 1, unsigned boatLength = 1, float maxAngle = 45.f, float maxSpeed = 60.f, unsigned proximity = 2) 
-            : MAP_WIDTH(mapWidth), MAP_HEIGHT(mapHeight), BOAT_WIDTH(boatWidth), BOAT_LENGTH(boatLength), MAX_ANGLE(maxAngle), MAX_SPEED(maxSpeed), PROXIMITY(proximity) { }
+        Settings(unsigned mapWidth, unsigned mapHeight, unsigned boatWidth = 1, unsigned boatLength = 1, float maxAngle = 45.f, float maxSpeed = 60.f, unsigned proximity = 2, unsigned step = 2) 
+            : MAP_WIDTH(mapWidth), MAP_HEIGHT(mapHeight), BOAT_WIDTH(boatWidth), BOAT_LENGTH(boatLength), MAX_ANGLE(maxAngle), MAX_SPEED(maxSpeed), PROXIMITY(proximity), STEP(step) { }
         
         ~Settings() { }
 
